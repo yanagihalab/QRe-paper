d343c57ca4ba1effe7cbfe4b12423036bd2ec3ba
keisho <key0008.sum@gmail.com>
2025-10-04 20:40:23 +0900

    start_up script

---
+++ api/start_qr.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+VENV_PATH="/home/yama/Desktop/QRe-paper/venv"
+LOG_FILE="/home/yama/qr_log.txt"
+
+echo "--- start.sh exe: $(date) ---" >> $LOG_FILE
+
+sleep 10
+echo "10 seconds waited" >> $LOG_FILE
+
+cd /home/yama/Desktop/QRe-paper/api
+echo " change dir $(pwd)" >> $LOG_FILE
+
+#. "${VENV_PATH}/bin/activate"
+#echo "venv activate" >> $LOG_FILE
+
+echo " try qr_code_display.py" >> $LOG_FILE
+#sudo -u yama /usr/bin/python qr_code_display.py >> $LOG_FILE 2>&1 &
+# tyokusetu venv no python wo yobu
+sudo -u yama "${VENV_PATH}/bin/python" qr_code_display.py >> $LOG_FILE 2>&1 &
+
+echo "--- complete start.sh ---" >> $LOG_FILE
+
+
